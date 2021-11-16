80ce0e1a5eedf53bc9e6ac2562dba11bdb39161c
Thiago Ricardo <thiago.tec.ricardo@gmail.com>
2021-11-16 11:24:38 -0300

    Adding modal-dialog-error

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { Courses } from '../model/courses';
 import { CoursesService } from './../services/courses.service';
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -16,12 +19,28 @@ export class CoursesComponent implements OnInit {
 
 
 
-  constructor(private coursesService: CoursesService) {
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+    ) {
     //this.courses
 
-    this.courses$ = this.coursesService.list();
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError (erroe => {
+        this.onError('Erro ao carregar cursos.');
+        return of([])
+      })
 
-  }
+    );
+
+    }
+
+    onError(errorMsg: string) {
+      this.dialog.open(ErrorDialogComponent, {
+        data: errorMsg
+      });
+    }
 
   ngOnInit(): void {
     //this.courses = this.coursesService.list();
--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -3,7 +3,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
@@ -11,7 +12,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatCardModule,
     MatTableModule,
     MatToolbarModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatButtonModule
   ],
 
 })
