2f84ee628a1cbf03cdecd7a90a4d95e6f9ebbf11
Thiago Ricardo <thiago.tec.ricardo@gmail.com>
2021-12-28 22:10:51 -0300

    remove import

--- crud-spring/src/main/java/com/tec/model/Course.java
+++ crud-spring/src/main/java/com/tec/model/Course.java
@@ -5,23 +5,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 
 @Entity
 public class Course {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@JsonProperty("_id")
 	private Long id;
-	
-	
+
+
 	@Column(length = 200, nullable = false)
 	private String name;
-	
+
 	@Column(length = 10, nullable = false)
 	private String category;
 
@@ -48,8 +46,8 @@ public class Course {
 	public void setCategory(String category) {
 		this.category = category;
 	}
-	
-	
-	
+
+
+
 
 }
